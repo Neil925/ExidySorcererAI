47316fbbb937872a18991b2826541d6a5aab8360
Neil Elkadi <elkadineil@gmail.com>
2024-02-29 16:50:25 -0500

    First working version

--- background.js
+++ background.js
@@ -1,12 +1,13 @@
-const Browser = require("webextension-polyfill");
+let id = -1;
 
-Browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (!tab.url?.includes("google.com/search"))
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (id == tabId || !tab.url?.includes("google.com/search"))
         return;
+
+    id = tabId;
 
     let splitSearch = tab.url.split("/search");
     let newSearch = "https://www.bing.com/search/" + splitSearch[1];
 
-    tab.url = newSearch;
-    Browser.tabs.create({ url: newSearch });
+    browser.tabs.update(id, { url: newSearch });
 });
