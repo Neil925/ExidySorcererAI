3a874b6f70452ed08418ebfddc98a9401aeb6378
Thomas-Jarrett1999 <tjfortyfour@gmail.com>
2024-03-14 00:33:33 -0400

    Random Search Engine Redirect Works
    The random search engine redirect now works, still need to add checks to stop redirect to the same search engine that is currently being used.

--- events/tab-update/go-to-bing.js
+++ events/tab-update/go-to-bing.js
@@ -1,29 +1,38 @@
 let id = -1;
-let searchURLS = ["google.com/search", "bing.com/search", "duckduckgo.com/?q="];
+let searchURLS = ["google.com/search?q=", "bing.com/search?q=", "duckduckgo.com/?q="];
 
 export default {
     porbability: 1,
     preventOtherEvents: true,
     //Either loading or complete
     stage: "loading",
-    runForUrls: ["google.com/search"],// "bing.com/search", "duckduckgo.com/?q="],
+    runForUrls: ["google.com/search?q=", "bing.com/search?q=", "duckduckgo.com/?q="],
     excemptUrls: [],
+    /**
+    * @param {number} tabId 
+    * @param {import("webextension-polyfill").Tabs.OnUpdatedChangeInfoType} changeInfo
+    * @param {import("webextension-polyfill").Tabs.Tab} tab
+    */
     execute: async (tabId, changeInfo, tab) => {
+        if (changeInfo.url == undefined) return;
+
         if (tabId == id)
             return;
 
         console.debug(changeInfo);
 
-        let splitSearch = tab.url.split("/search");
+        let splitSearch = tab.url.split("/search?q=");
 
-        if (splitSearch == undefined)
+        console.debug(splitSearch);
+
+        if (splitSearch.length == 1)
             splitSearch = tab.url.split("/?q=");
 
         //randomly generate either 0 or 1 
         let randNum = Math.floor(Math.random() * 3);
 
         //set newSearch to an empty string 
-        let newSearch = "http://www." + searchURLS[0] + splitSearch[1];
+        let newSearch = "http://www." + searchURLS[randNum] + splitSearch[1];
 
         id = tabId;
         setTimeout(() => id = -1, 3000);
