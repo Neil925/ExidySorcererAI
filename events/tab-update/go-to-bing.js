1ad6a4feeda72ff7ab1cebdc83f5fc6b1a307ddd
Thomas-Jarrett1999 <tjfortyfour@gmail.com>
2024-03-13 23:17:20 -0400

    Initial Commit
    Initial commit for this Branch, code is not working yet but I needed to run tests on main.

--- events/tab-update/go-to-bing.js
+++ events/tab-update/go-to-bing.js
@@ -1,11 +1,12 @@
 let id = -1;
+let searchURLS = ["google.com/search", "bing.com/search", "duckduckgo.com/?q="];
 
 export default {
     porbability: 1,
     preventOtherEvents: true,
     //Either loading or complete
     stage: "loading",
-    runForUrls: ["google.com/search"],
+    runForUrls: ["google.com/search"],// "bing.com/search", "duckduckgo.com/?q="],
     excemptUrls: [],
     execute: async (tabId, changeInfo, tab) => {
         if (tabId == id)
@@ -14,7 +15,15 @@ export default {
         console.debug(changeInfo);
 
         let splitSearch = tab.url.split("/search");
-        let newSearch = "https://www.bing.com/search/" + splitSearch[1];
+
+        if (splitSearch == undefined)
+            splitSearch = tab.url.split("/?q=");
+
+        //randomly generate either 0 or 1 
+        let randNum = Math.floor(Math.random() * 3);
+
+        //set newSearch to an empty string 
+        let newSearch = "http://www." + searchURLS[0] + splitSearch[1];
 
         id = tabId;
         setTimeout(() => id = -1, 3000);
